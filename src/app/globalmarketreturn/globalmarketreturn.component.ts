35ef56e028f881cebdc6406fcd13ba04c606a4fc
Dmitry Kravchenko <dkravchenko@outlook.com>
2023-08-26 17:03:03 +0200

    msci real and nominal returns calcualted

--- src/app/globalmarketreturn/globalmarketreturn.component.ts
+++ src/app/globalmarketreturn/globalmarketreturn.component.ts
@@ -1,9 +1,9 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartComponent } from 'ng-apexcharts';
 import { ChartOptions } from '../additionalcost/additionalcost.component';
 import { IHistoricalRate } from './ihistoricalrate';
-import msciAcwiIndex from 'src/assets/msciacwiindex.json';
-import vpiInflationMonthly from 'src/assets/vpiinflationmonthly.json';
+import msciAcwiIndex from '../../assets/msciacwiindex.json';
+import vpiInflationMonthly from '../../assets/vpiinflationmonthly.json';
 import { IMortageLoanData } from './imortageloandata';
 import { IAggregatedMortageLoanData } from './iaggregatedmortageloandate';
 
@@ -12,7 +12,7 @@ import { IAggregatedMortageLoanData } from './iaggregatedmortageloandate';
   templateUrl: './globalmarketreturn.component.html',
   styleUrls: ['./globalmarketreturn.component.css']
 })
-export class GlobalMarketReturnComponent implements OnInit{
+export class GlobalMarketReturnComponent implements OnInit {
 
   @ViewChild("chart") chart!: ChartComponent;
   public chartOptions!: Partial<ChartOptions> | any;
@@ -22,6 +22,10 @@ export class GlobalMarketReturnComponent implements OnInit{
   inflation!: IHistoricalRate;
 
   aggregatedCreditData: IAggregatedMortageLoanData = {} as IAggregatedMortageLoanData;
+  overallReturnNominal: number = 0.0;
+  overallReturnReal: number = 0.0;
+  annualizedReturnReal: number = 0.0;
+  annualizedReturnNominal: number = 0.0;
 
   constructor() {
 
@@ -30,28 +34,28 @@ export class GlobalMarketReturnComponent implements OnInit{
       closingCost: 20000,
       loanAmount: 100000,
       startDate: new Date("1991-01"),
-      endDate: new Date("2021-01"),
+      endDate: new Date("2020-12"),
     };
 
-    this.aggregatedCreditData =  {
-      mortageIntervals : [creditDataItem],
-      downPayment: 100000,
+    this.aggregatedCreditData = {
+      mortageIntervals: [creditDataItem],
+      downPayment: 10000,
     }
 
     this.msciDevelopmentNominal = {
-      name:'MSCI ACWI',
+      name: 'MSCI ACWI',
       date: [],
       rate: [],
     };
 
     this.msciDevelopmentReal = {
-      name:'MSCI ACWI inflation angepasst',
+      name: 'MSCI ACWI inflation angepasst',
       date: [],
       rate: [],
     };
 
     this.msciDevelopmentRealTer = {
-      name:'MSCI ACWI inflation angepasst mit 1% Nebenkosten',
+      name: 'MSCI ACWI inflation angepasst mit 1% Nebenkosten',
       date: [],
       rate: [],
     };
@@ -59,7 +63,7 @@ export class GlobalMarketReturnComponent implements OnInit{
     this.chartOptions = {
       chart: {
         type: "line",
-        height: 400,
+        height: 350,
         toolbar: {
           show: false
         },
@@ -97,60 +101,85 @@ export class GlobalMarketReturnComponent implements OnInit{
       },
       yaxis: {
         labels: {
-          formatter: function(value: number) {
+          formatter: function (value: number) {
             return (Math.round(value * 100) / 100).toFixed(2);
           }
         },
       },
     };
-   }
+  }
 
   ngOnInit(): void {
     this.getMcsiAcwiData();
   }
 
-  getMcsiAcwiData() : void {
+  getMcsiAcwiData(): void {
 
     //TODO think about loop here cause of refinancing your credit
-    var startDate = this.aggregatedCreditData.mortageIntervals[0].startDate;
-    var endDate = this.aggregatedCreditData.mortageIntervals[0].endDate;
-    var closingCosts = this.aggregatedCreditData.mortageIntervals[0].closingCost;
-    var interestRate = this.aggregatedCreditData.mortageIntervals[0].annualPercentageRate;
-    var loanAmount = this.aggregatedCreditData.mortageIntervals[0].loanAmount;
+    const startDate = this.aggregatedCreditData.mortageIntervals[0].startDate;
+    const startIndex = msciAcwiIndex.findIndex(msciAcwi => new Date(msciAcwi.Date).getTime() === startDate.getTime());
 
-    var startIndex = msciAcwiIndex.findIndex(msciAcwi =>  new Date(msciAcwi.Date).getTime() === startDate.getTime());
-    var endIndex = msciAcwiIndex.findIndex(msciAcwi =>  new Date(msciAcwi.Date).getTime() === endDate.getTime());
+    const endDate = this.aggregatedCreditData.mortageIntervals[0].endDate;
+    const endIndex = msciAcwiIndex.findIndex(msciAcwi => new Date(msciAcwi.Date).getTime() === endDate.getTime());
 
-    //Monthly Payment = P * (r * (1 + r)^n) / ((1 + r)^n - 1)
+    // Tt is a simple iterest, not a compound
+    // Monthly Payment = P * (r * (1 + r)^n) / ((1 + r)^n - 1)
     // Where:
     // P = Principal amount (loan amount) = $200,000
     // r = Monthly interest rate = 0.00375
     // n = Total number of payments (loan term in months) = 30 years * 12 months/year = 360 months
-    var monthlyRate = interestRate/100/12;
-    var totalNumberPayments = 12* (endDate.getFullYear() - startDate.getFullYear()) + endDate.getMonth() - startDate.getMonth();
-    var monthlyInterest = loanAmount * (monthlyRate * Math.pow(1 + monthlyRate, totalNumberPayments)) / Math.pow(1 + monthlyRate, totalNumberPayments - 1);
-    var initialInvestmentNominal = closingCosts +  this.aggregatedCreditData.downPayment;
-    var initialInvestmentReal = initialInvestmentNominal;
+    const loanAmount = this.aggregatedCreditData.mortageIntervals[0].loanAmount;
+    const monthlyInterestRate = this.aggregatedCreditData.mortageIntervals[0].annualPercentageRate / 100 / 12;
+    const paymentsNumberInMonth = 12 * (endDate.getFullYear() - startDate.getFullYear()) + endDate.getMonth() - startDate.getMonth() + 1; // +1 cause we need to pay for the first month too
+    const monthlyInterest = loanAmount *
+      (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, paymentsNumberInMonth)) / (Math.pow(1 + monthlyInterestRate, paymentsNumberInMonth) - 1);
 
-    this.msciDevelopmentNominal.rate.push(initialInvestmentNominal);
+    const initialCost = this.aggregatedCreditData.mortageIntervals[0].closingCost + this.aggregatedCreditData.downPayment;
+    var capitalGainNominal = initialCost + monthlyInterest; // therefore i = startIndex + 1 we start from index + 1, however we have a still initial payment
+    var capitalGainReal = capitalGainNominal;
+
+    this.msciDevelopmentNominal.rate.push(capitalGainNominal);
     this.msciDevelopmentNominal.date.push(msciAcwiIndex[startIndex].Date);
 
-    this.msciDevelopmentReal.rate.push(initialInvestmentNominal);
+    this.msciDevelopmentReal.rate.push(capitalGainReal);
     this.msciDevelopmentReal.date.push(msciAcwiIndex[startIndex].Date);
 
-    for(var i = startIndex + 1; i < endIndex; i++) {
-      initialInvestmentNominal = (monthlyInterest + initialInvestmentNominal) * (1 +  msciAcwiIndex[i].MoM);
+    var monthlyPaymentsReal = monthlyInterest; // in the very first month we do not have inflation MoM, therefore i = startIndex + 1 we start from index + 1, however we have a still initial payment
+    var totalInitialPaymentReal = initialCost;
+
+    for (let i = startIndex + 1; i <= endIndex; i++) {
+      // we do not apply inflation for the  monthly interest at the current month, but to the previous month cause inflation is Month-over-Month
+      monthlyPaymentsReal = monthlyPaymentsReal * (1 - vpiInflationMonthly[i]!.InflationMoM! / 100) + monthlyInterest;
+      totalInitialPaymentReal = totalInitialPaymentReal * (1 - vpiInflationMonthly[i]!.InflationMoM! / 100);
+
+      // we do not apply market gain for the  monthly investment at the current month, but to the previous month cause market gain is Month-over-Month
+      capitalGainNominal = capitalGainNominal * (1 + msciAcwiIndex[i].MoM) + monthlyInterest;
       this.msciDevelopmentNominal.date.push(msciAcwiIndex[i].Date);
-      this.msciDevelopmentNominal.rate.push(initialInvestmentNominal);
+      this.msciDevelopmentNominal.rate.push(capitalGainNominal);
 
-      var iflationRateAdjustment = vpiInflationMonthly[i]!.InflationMoM!/100 * initialInvestmentReal;
-      initialInvestmentReal = (monthlyInterest + initialInvestmentReal) * (1 +  msciAcwiIndex[i].MoM) - iflationRateAdjustment;
-
+      capitalGainReal = capitalGainReal * (1 + msciAcwiIndex[i].MoM) * (1 - vpiInflationMonthly[i]!.InflationMoM! / 100) + monthlyInterest;
       this.msciDevelopmentReal.date.push(msciAcwiIndex[i].Date);
-      this.msciDevelopmentReal.rate.push(initialInvestmentReal);
+      this.msciDevelopmentReal.rate.push(capitalGainReal);
 
       // this.msciDevelopmentRealTer.date.push(msciAcwiIndex[i].Date);
       // this.msciDevelopmentRealTer.rate.push(initialInvestmentReal);
     }
+
+    const paymentsNumberInYears = paymentsNumberInMonth / 12;
+
+    this.overallReturnReal = Math.round((this.msciDevelopmentReal.rate[endIndex] + Number.EPSILON) * 100) / 100;
+    const totalInvestementReal = totalInitialPaymentReal + monthlyPaymentsReal;
+    this.annualizedReturnReal = this.calculateAnnualizedReturn(this.msciDevelopmentReal.rate[endIndex], totalInvestementReal, paymentsNumberInYears);
+
+    this.overallReturnNominal = Math.round((this.msciDevelopmentNominal.rate[endIndex] + Number.EPSILON) * 100) / 100;
+    const totalInvestementNominal = initialCost + monthlyInterest * paymentsNumberInMonth;
+    this.annualizedReturnNominal = this.calculateAnnualizedReturn(this.msciDevelopmentNominal.rate[endIndex], totalInvestementNominal, paymentsNumberInYears);
+  }
+
+  private calculateAnnualizedReturn(capitalGain: number, investedAmount: number, duration: number) {
+    // annualized return, also known as the compound annual growth rate
+    // CAGR = ( Final Value / Initial Investment ​) ^ (1 / Number of Years) − 1
+    var returnPerAnnum = (Math.pow(capitalGain / investedAmount, 1 / duration) - 1) * 100;
+    return Math.round((returnPerAnnum + Number.EPSILON) * 100) / 100;
   }
 }

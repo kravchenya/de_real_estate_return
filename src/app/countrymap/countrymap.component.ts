105c09ba8a6ee00c470be818e0a9fb05e8025b24
Dmitry Kravchenko <dkravchenko@outlook.com>
2023-08-28 08:44:20 +0200

    german's map alllocation was improved

--- src/app/countrymap/countrymap.component.ts
+++ src/app/countrymap/countrymap.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
 import additionalCosts from 'src/assets/additionalcosts.json';
@@ -8,20 +8,23 @@ import additionalCosts from 'src/assets/additionalcosts.json';
   templateUrl: './countrymap.component.html',
   styleUrls: ['./countrymap.component.css']
 })
-export class CountrymapComponent implements OnInit {
+export class CountrymapComponent implements OnInit, AfterViewInit {
 
   style = 'mapbox://styles/mapbox/light-v11';
   lat = 51;
   lng = 10;
 
-  public landAcquisition2: string = '';
-  federalState2: string = '';
-  notary2: string = '';
-  realtor2: string = '';
+  public landAcquisition: string = '';
+  federalState: string = '';
+  notary: string = '';
+  realtor: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
     var map = new mapboxgl.Map({
       container: 'mapElement',
       accessToken: environment.mapbox.accessToken,
@@ -31,15 +34,8 @@ export class CountrymapComponent implements OnInit {
     });
 
     map.addControl(new mapboxgl.NavigationControl());
-    // map.addControl(new mapboxgl.FullscreenControl());
 
     var hoveredStateId = 0;
-
-    // Target the span elements used in the sidebar
-    var federalState = document.getElementById('federalState');
-    var landAcquisition = document.getElementById('landAcquisition');
-    var notary = document.getElementById('notary');
-    var realtor = document.getElementById('realtor');
 
     map.on('load', () => {
       map.addSource('bundeslaender', {
@@ -96,13 +92,13 @@ export class CountrymapComponent implements OnInit {
           );
 
           // federalState!.textContent = e.features[0].properties.name;
-          this.federalState2 = e.features[0].properties.name;
-          this.landAcquisition2 = additionalCosts[hoveredStateId].properties.landAcquisition + '%';
+          this.federalState = e.features[0].properties.name;
+          this.landAcquisition = additionalCosts[hoveredStateId].properties.landAcquisition + '%';
           //landAcquisition!.textContent = additionalCosts[hoveredStateId].properties.landAcquisition  + '%';
           // notary!.textContent = 'rund ' + additionalCosts[hoveredStateId].properties.notary  + '%';
-          this.notary2! = 'rund ' + additionalCosts[hoveredStateId].properties.notary + '%';
+          this.notary! = 'rund ' + additionalCosts[hoveredStateId].properties.notary + '%';
           // realtor!.textContent =  additionalCosts[hoveredStateId].properties.realtor  + '%';
-          this.realtor2! = additionalCosts[hoveredStateId].properties.realtor + '%';
+          this.realtor! = additionalCosts[hoveredStateId].properties.realtor + '%';
 
         }
       });
@@ -118,15 +114,10 @@ export class CountrymapComponent implements OnInit {
         }
         hoveredStateId = 0;
 
-        // federalState!.textContent = '';
-        // landAcquisition!.textContent = '';
-        // notary!.textContent = '';
-        // realtor!.textContent = '';
-
-        this.federalState2 = '';
-        this.landAcquisition2 = '';
-        this.notary2 = '';
-        this.realtor2 = '';
+        this.federalState = '';
+        this.landAcquisition = '';
+        this.notary = '';
+        this.realtor = '';
       });
 
       map.on('idle', () => {

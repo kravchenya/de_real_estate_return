c1e91f842c8de4f271555eb1820ebce4329f59f1
Dmitry Kravchenko <dkravchenko@outlook.com>
2023-09-01 08:38:35 +0200

    renamed ReDe2 to ReDe

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ReDe2'),
+      dir: require('path').join(__dirname, './coverage/ReDe'),
       subdir: '.',
       reporters: [
         { type: 'html' },
